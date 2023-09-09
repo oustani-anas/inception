690fdcdedce180286e7fbda198268969f8ea9220
Anas Oustani <aoustani@e3r10p10.1337.ma>
2023-09-09 15:45:19 +0100

    fixed mariadb

--- mariadb/script.sh
+++ mariadb/script.sh
@@ -1,3 +1,9 @@
-
 #!/bin/bash
 
+service mariadb start
+    
+mysql  -e "CREATE DATABASE $MYSQL_DATABASE;" 
+mysql  -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql  -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'%';"
+mysql  -e "FLUSH PRIVILEGES;"
+tail -f
