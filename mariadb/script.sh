3a93234a9518b45f2061c77e64e3442588c90516
oustani-anas <anas.migos20@gmail.com>
2023-09-20 05:41:04 +0100

    link between wordpress and database(maria)

--- mariadb/script.sh
+++ mariadb/script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 service mariadb start
     
-mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" 
-mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* to '$SQL_USER'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
-tail -f
+mariadb -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" 
+mariadb -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+mariadb -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* to '$SQL_USER'@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
+service mariadb stop
+
+mysqld_safe
+# tail -f
--- wordpress/wp_script.sh
+++ wordpress/wp_script.sh
@@ -1,11 +1,12 @@
-
 #!/bin/bash
+
+mkdir  -p /var/www/wordpress
+
 cd /var/www/wordpress
 
 wp core download --allow-root
 
 cp wp-config-sample.php wp-config.php
-
 # echo "this infos = $SQL_DATABASE $SQL_PASSWORD $SQL_USER"
 
 wp config set DB_NAME $SQL_DATABASE --allow-root 
@@ -14,8 +15,6 @@ wp config set DB_PASSWORD $SQL_PASSWORD --allow-root
 wp config set DB_HOST mariadb --allow-root 
 
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
 
 tail -f
