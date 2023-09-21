5ddf105205cfffcf229922f3309c991ca722694c
oustani-anas <anas.migos20@gmail.com>
2023-09-21 02:46:24 +0100

    the site is work (hoome page)

--- mariadb/script.sh
+++ mariadb/script.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-service mariadb start
+
+
+# sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+# service mariadb start
     
-mariadb -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" 
-mariadb -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* to '$SQL_USER'@'%';"
-mariadb -e "FLUSH PRIVILEGES;"
-service mariadb stop
+# mariadb -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" 
+# mariadb -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+# mariadb -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* to '$SQL_USER'@'%';"
+# mariadb -e "FLUSH PRIVILEGES;"
+
+# mysqladmin -u root shutdown
+# mysqld_safe
+
+
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+service mariadb start
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
+mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* to '$SQL_USER'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root shutdown
 
 mysqld_safe
 # tail -f
--- wordpress/wp_script.sh
+++ wordpress/wp_script.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-mkdir  -p /var/www/wordpress
+sleep 30
 
-cd /var/www/wordpress
+sed -i -e 's/listen =.*/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
 
-wp core download --allow-root
+
+
+cd /var/www/html/wordpress
+
+wp core download --allow-root --path=/var/www/html/wordpress
 
 cp wp-config-sample.php wp-config.php
 # echo "this infos = $SQL_DATABASE $SQL_PASSWORD $SQL_USER"
 
-wp config set DB_NAME $SQL_DATABASE --allow-root 
-wp config set DB_USER $SQL_USER --allow-root 
-wp config set DB_PASSWORD $SQL_PASSWORD --allow-root 
-wp config set DB_HOST mariadb --allow-root 
+wp config set DB_NAME $SQL_DATABASE --allow-root --path=/var/www/html/wordpress
+wp config set DB_USER $SQL_USER --allow-root --path=/var/www/html/wordpress
+wp config set DB_PASSWORD $SQL_PASSWORD --allow-root --path=/var/www/html/wordpress
+wp config set DB_HOST mariadb --allow-root --path=/var/www/html/wordpress
 
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root --path=/var/www/html/wordpress
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root --path=/var/www/html/wordpress
 
-tail -f
+mkdir -p /run/php
+
+php-fpm7.4 -F
