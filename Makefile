614d3966af43485423d67954aab06b4cfc62b381
oustani-anas <anas.migos20@gmail.com>
2023-09-26 01:19:23 +0100

    the final

--- Makefile
+++ Makefile
@@ -1,2 +1,15 @@
 
+SRC = srcs/docker-compose.yml
 
+all: up
+
+up:
+	mkdir -p /Users/aoustani/Desktop/incep/db
+	mkdir -p /Users/aoustani/Desktop/incep/wp
+	docker-compose -f $(SRC) up --build -d
+
+fclean:
+	docker-compose -f $(SRC) down
+	docker system prune -f -all --volumes
+	rm -rf /Users/aoustani/Desktop/incep/wp
+	rm -rf /Users/aoustani/Desktop/incep/db
--- srcs/requirements/wordpress/wp_script.sh
+++ srcs/requirements/wordpress/wp_script.sh
@@ -13,7 +13,7 @@ cp wp-config-sample.php wp-config.php
 wp config set DB_NAME $SQL_DATABASE --allow-root
 wp config set DB_USER $SQL_USER --allow-root 
 wp config set DB_PASSWORD $SQL_PASSWORD --allow-root 
-wp config set DB_HOST mariadb --allow-root 
+wp config set DB_HOST $SQL_HOST --allow-root 
 
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root --path=/var/www/html/wordpress
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root --path=/var/www/html/wordpress
