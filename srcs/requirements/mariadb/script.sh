99d55b12e27b65c7cbe73738cec9b52c781c4251
oustani-anas <anas.migos20@gmail.com>
2023-10-02 12:18:27 +0100

    okok testing

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
@@ -7,7 +7,7 @@ service mariadb start
 mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
 mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* to '$SQL_USER'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
+# mysql -e "FLUSH PRIVILEGES;"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD' ;"
 
 mysqladmin -u root --password=$SQL_ROOT_PASSWORD shutdown
