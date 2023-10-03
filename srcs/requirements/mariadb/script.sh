db1e3aef99a2e66c49fc3221aed6547fae18a9d3
oustani-anas <anas.migos20@gmail.com>
2023-10-03 22:43:28 +0100

    okok

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -7,7 +7,7 @@ service mariadb start
 mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
 mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* to '$SQL_USER'@'%';"
-# mysql -e "FLUSH PRIVILEGES;"
+mysql -e "FLUSH PRIVILEGES;"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD' ;"
 
 mysqladmin -u root --password=$SQL_ROOT_PASSWORD shutdown
